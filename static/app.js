7a82fede2f2f05084e0438c47875dca9c2ee0114
dbdbdeep365 <ytw618@gmail.com>
2024-06-25 06:29:49 +0900

    memo test

---
+++ static/app.js
@@ -0,0 +1,43 @@
+function displayMemo(memo) {
+  const ul = document.querySelector("#memo-ul");
+  const li = document.createElement("li");
+  li.innerText = `[id:${memo.id}] ${memo.content}`;
+  ul.appendChild(li);
+}
+
+async function readMemo() {
+  const res = await fetch("/memos");
+  const jsonRes = await res.json();
+  const ul = document.querySelector("#memo-ul");
+  ul.innerHTML = "";
+  //jsonRes = [{id:123, content'블라블라'}]
+  jsonRes.forEach(displayMemo);
+}
+
+readMemo();
+
+async function createMemo(value) {
+  const res = await fetch("/memos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: new Date().getTime(),
+      content: value,
+    }),
+  });
+  readMemo();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = document.querySelector("#memo-input");
+  createMemo(input.value);
+  input.value = "";
+}
+
+const form = document.querySelector("#memo-form");
+form.addEventListener("submit", handleSubmit);
+
+readMemo();
