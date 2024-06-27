821533023d74642f0b15602212a61aa9affb593f
dbdbdeep365 <ytw618@gmail.com>
2024-06-27 22:52:32 +0900

    memo_2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 class Memo(BaseModel):
-    id:str
+    id:int
     content:str
 
 memos = []
@@ -19,4 +19,24 @@ def create_memo(memo:Memo):
 def read_memo():
     return memos
 
+@app.put("/memos/{memo_id}")
+def put_memo(req_memo:Memo):
+    for memo in memos:
+        if memo.id ==req_memo.id:
+            memo.content=req_memo.content
+            return '성공했습니다.'
+    return '그런 메모는 없습니다.'
+    
+    
+@app.delete("/memos/{memo_id}")
+def delete_memo(memo_id:int):
+    for index,memo in enumerate(memos):
+        if memo.id ==memo_id:
+             memos.pop(index)
+             return '성공했습니다.'
+    return '그런 메모는 없습니다.'
+       
+
+
 app.mount("/",StaticFiles(directory='static',html=True), name='static')
+
--- static/app.js
+++ static/app.js
@@ -1,8 +1,44 @@
+async function editMemo(event) {
+  const id = event.target.dataset.id;
+  const editInput = prompt("수정할 값을 입력하세요.");
+  const res = await fetch(`/memos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: id,
+      content: editInput,
+    }),
+  });
+  readMemo();
+}
+
+async function deleteMemo(event) {
+  const id = event.target.dataset.id;
+  const res = await fetch(`/memos/${id}`, {
+    method: "DELETE",
+  });
+  readMemo();
+}
+
 function displayMemo(memo) {
   const ul = document.querySelector("#memo-ul");
   const li = document.createElement("li");
-  li.innerText = `[id:${memo.id}] ${memo.content}`;
+  li.innerText = `${memo.content}`;
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "수정하기.";
+  editBtn.addEventListener("click", editMemo);
+  editBtn.dataset.id = memo.id;
+
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "삭제";
+  delBtn.addEventListener("click", deleteMemo);
+  delBtn.dataset.id = memo.id;
+
   ul.appendChild(li);
+  li.appendChild(editBtn);
+  li.appendChild(delBtn);
 }
 
 async function readMemo() {
